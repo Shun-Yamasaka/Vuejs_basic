dd7e370e29ffc50a80639e42c5b19246afb7d183
shun yamasaka <118syuunico@gmail.com>
2023-01-05 20:19:29 +0900

    Vue.jsの基本１

---
+++ 2-1/script/script.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+    el:'#app',
+    data(){                     // data関数を定義
+        return {                // オブジェクトをreturnする
+            text:'hello Vue.js' // textプロパティに文字列を設定
+        }
+    }
+});
