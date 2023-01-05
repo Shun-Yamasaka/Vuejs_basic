a4a6edeb35d717a8d84786528528c91585cd5bd1
shun yamasaka <118syuunico@gmail.com>
2023-01-05 21:30:12 +0900

    カウンターアプリをVuejsを使って実装した例を作成

---
+++ 3-2/script/script.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    // data関数定義
+    data(){
+        return {
+            count:0 // カウントプロパティを準備
+        };
+    },
+    methods:{
+        // インクリメントメソッドを定義
+        increment(){
+            this.count++; // 処理
+        },
+        // デクリメントメソッドを定義
+        decrement(){
+            this.count--; // 処理
+        }
+    }
+});
